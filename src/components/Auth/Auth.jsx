b58498964e1cd334bba703e6f5a3d8f9448fc9ba
Slava <slava91petrushin@yandex.ru>
2020-03-19 19:57:12 +0300

    add auth form

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,10 +1,91 @@
 import React, {Component} from 'react';
+import Button from "../ui/Button/Button";
+import classes from './Auth.module.css'
+import Input from "../ui/Input/Input";
 
 class Auth extends Component {
+    state = {
+        formControls:{
+            email: {
+                value: '',
+                type: 'email',
+                label: 'Email',
+                shouldValidate: true,
+                errorMessage: 'Введите корректный email',
+                valid: false,
+                touched: false,
+                validation:{ //Правила для валидации
+                    required: true,
+                    email: true
+                }
+            },
+            password: {
+                value: '',
+                type: 'password',
+                label: 'Пароль',
+                shouldValidate: true,
+                errorMessage: 'Введите корректный пароль',
+                valid: false,
+                touched: false,
+                validation:{
+                    required: true,
+                    minLength: 6
+                }
+            }
+        }
+    };
+
+    loginHandler = () => {
+
+    };
+
+    registerHandler = () => {
+
+    };
+
+    submitForm = (e) => {
+        e.preventDefault();
+    };
+
+
+    onChangeHandler = (event, controlName) => {
+        const newInput = {
+            ...this.state.formControls[controlName],
+            value: event.target.value,
+            touched: true,
+        };
+        debugger
+
+        console.log(`${controlName} ${event.target.value}`)
+    };
+
+    renderInputs() {
+        return Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName];
+            return (
+                <Input
+                    {...control}
+                    key={controlName + index}
+                    shouldValidate={!!control.validation}
+                    onChange={event => this.onChangeHandler(event, controlName)}
+                />
+            )
+        });
+    }
+
     render() {
         return (
-            <div>
-                Auth
+            <div className={classes.Auth}>
+                <div>
+                    <h2>
+                        Авторизация
+                    </h2>
+                    <form onSubmit={this.submitForm} className={classes.AuthForm}>
+                        {this.renderInputs()}
+                        <Button type={'success'} onClick={this.loginHandler}>Авторизация</Button>
+                        <Button type={'primary'} onClick={this.registerHandler}>Регистрация</Button>
+                    </form>
+                </div>
             </div>
         );
     }
---
+++ src/components/ui/Input/Input.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import classes from './Input.module.css';
+
+function isInvalid({valid, touched, shouldValidate}) {//shouldValidate надо ли валидировать  touched - трогали ли
+    return !valid && shouldValidate && touched
+}
+
+const Input = props => {
+
+    const inputType = props.type || 'text';
+    const cls = [classes.Input];
+    const htmlFor = `${inputType}-${Math.random()}`;
+
+    if (isInvalid(props)) {
+        cls.push(classes.invalid)
+    }
+
+    return (
+        <div className={cls.join(' ')}>
+            <label htmlFor={htmlFor}>
+                {props.label}
+                <input
+                    type={inputType}
+                    id={htmlFor}
+                    value={props.value}
+                    onChange={props.onChange}
+                />
+            </label>
+            {
+                isInvalid(props)
+                    ? <span>{props.errorMessage || 'Введите верное значениеS'}</span>
+                    : null
+            }
+        </div>
+    )
+};
+
+export default Input;
